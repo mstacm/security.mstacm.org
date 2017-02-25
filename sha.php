0c3a3e6ec14481bc9e341997e159c5de1acebb56
bcurn37 <bdcurran37@gmail.com>
2017-02-24 20:26:20 -0600

    secret ctf stuff

---
+++ sha.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_GET['name']) and isset($_GET['password'])) {
+    $name = (string)$_GET['name'];
+    $password = (string)$_GET['password'];
+
+    if ($name == $password) {
+        print 'Your password can not be your name.';
+        print var_dump($name) . var_dump($password);
+    } else if (sha1($name) === sha1($password)) {
+        print 'flag'
+        print var_dump($name) . var_dump($password);
+    } else {
+        print '<p class="alert">Invalid password.</p>';
+        print var_dump($name) . var_dump($password);
+    }
+}
+?>
