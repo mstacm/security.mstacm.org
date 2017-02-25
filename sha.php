5ee3266c0f6a122f67300c715ba43b70e28a965c
bcurn37 <bdcurran37@gmail.com>
2017-02-24 20:31:47 -0600

    stuff

--- sha.php
+++ sha.php
@@ -5,10 +5,16 @@ if (isset($_GET['name']) and isset($_GET['password'])) {
 
     if ($name == $password) {
         print 'Your password can not be your name.';
+        print var_dump($password);
+        print var_dump($name);
     } else if (sha1($name) === sha1($password)) {
         print 'flag';
+        print var_dump($password);
+        print var_dump($name);
     } else {
         print '<p class="alert">Invalid password.</p>';
+        print var_dump($password);
+        print var_dump($name);
     }
 }
 ?>
