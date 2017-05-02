9533a1e92ded9c1c8760d484872847b247bf596c
root <root@acmsigsec.mst.edu>
2017-05-02 16:48:13 -0500

    [Fix] Minor security tweaks

--- deploy.php
+++ deploy.php
@@ -8,13 +8,8 @@
 
     // array of commands
     $commands = array(
-        'echo $PWD',
-        'whoami',
         'git pull',
         'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
     );
 
     // exec commands
