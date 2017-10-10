6d913fdcb51a8ba55ed3e1eaa14b275457b874b6
Hannah Bolt (LoneGalaxy) <hannah@master-logic.com>
2017-10-09 23:28:42 -0500

    Testing this auto close

--- myapp/server.js
+++ myapp/server.js
@@ -68,7 +68,7 @@ RowsCheck.load({
       throw err;
     }
     //Checks the spreadsheet to make sure it has enough free rows
-    else if(info.totalRows > 5){
+    else if(info.lastRow > 3){
       console.log("Registration is full! All rows used.");
       res.redirect('https://acmsigsec.mst.edu/myapp/website/regFull.html');
     }
