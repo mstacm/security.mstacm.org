88b83f522ff083f0209c1663c9d713217f3eb26e
Portal2Mtr <crfmb@umsystem.edu>
2020-10-08 15:29:45 -0500

    Moving nodejs scripts to test.

---
+++ node/app.js
@@ -0,0 +1,37 @@
+
+const express = require('express')
+const app = express();
+
+app.use(express.json());
+
+const courses=[
+   {id:1,name:'course1'},
+   {id:2,name:'course2'}
+];
+
+app.get('/',(req,res) =>{
+   res.send('Hello World!!!');
+});
+
+app.get('/api/courses',(req,res)=>{
+   res.send([1,2,3]);
+});
+
+// /api/courses/1
+app.get('/api/courses/:id',((req, res) =>{
+   res.send(req.params.id);
+}));
+
+app.post('/api/courses',(req,res) =>{
+   const course = {
+      id:courses.length+1,
+      name:req.body.name,
+   };
+   courses.push(course);
+   res.send(course);
+});
+
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(port,()=> console.log(`Listening on port ${port}!`));
