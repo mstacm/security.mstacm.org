d6d6657c6a78b503e14746e233ae35f8c3467978
Portal2Mtr <crfmb@umsystem.edu>
2020-10-08 16:03:02 -0500

    Testing node apo

--- node/index.js
+++ node/index.js
@@ -9,28 +9,31 @@ const courses=[
     {id:2,name:'course2'}
 ];
 
-app.get('/',(req,res) =>{
-    res.send('Hello World!!!');
-});
+// app.get('/',(req,res) =>{
+//     res.send('Hello World!!!');
+// });
+//
+// app.get('/api/courses',(req,res)=>{
+//     res.send([1,2,3]);
+// });
+//
+// // /api/courses/1
+// app.get('/api/courses/:id',((req, res) =>{
+//     res.send(req.params.id);
+// }));
+//
+// app.post('/api/courses',(req,res) =>{
+//     const course = {
+//         id:courses.length+1,
+//         name:req.body.name,
+//     };
+//     courses.push(course);
+//     res.send(course);
+// });
 
-app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3]);
-});
-
-// /api/courses/1
-app.get('/api/courses/:id',((req, res) =>{
-    res.send(req.params.id);
+app.get('/emaillists',((req, res) =>{
+    res.send('It Works!');
 }));
-
-app.post('/api/courses',(req,res) =>{
-    const course = {
-        id:courses.length+1,
-        name:req.body.name,
-    };
-    courses.push(course);
-    res.send(course);
-});
-
 
 // PORT
 const port = process.env.PORT || 3000
