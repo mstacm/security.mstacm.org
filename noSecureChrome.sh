e5ba69867420d9df7ed5609fdef94e53e78396d5
Portal2Mtr <crfmb@umsystem.edu>
2020-10-26 18:52:54 -0500

    Finalized payment form (finally!), working on backend next.

---
+++ noSecureChrome.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+google-chrome --disable-web-security --user-data-dir=~/
--- node/registerEvents/paymentPages/clientPay.js
+++ node/registerEvents/paymentPages/clientPay.js
@@ -36,47 +36,82 @@ function generateUserForm(clientInfo){
 }
 
 function makeTransaction(token){
-    userdata = []
+    // TODO Send token to server
+    // result.token.id
 
-    for (i in forms){
-        var key = forms[i].id
-        var obj = {}
-        obj[key] =$('#'+forms[i].id).val()
-        userdata.push(obj)
-    }
-    obj = {"token":token}
-    userdata.unshift(obj)
-
-    $.ajax({
-        type:'POST',
-        url:'http://localhost:3001/regCharge',
-        data:JSON.stringify({data:userdata}),
-        processData: false,
-        contentType: 'application/json',
-
-        success: function(json) {
-            json = JSON.parse(json)
-            const trxnSuccess = json.trxnSuccess
-
-            if (json.success){
-                alert("Success! You are registered! " + trxnSuccess + " You will be redirected shortly!");
-                window.setTimeout(function(){
-                    // Move to a new location or you can do something else
-                    window.location.href = "https://acmsec.mst.edu/events/";
-                }, 5000);
-            }else{
-                alert("Uh-oh! Something went wrong. " + trxnSuccess + " Refresh and try again!");
-            }
-
-        },
-
-        error: function() {
-            alert("Error! Please refresh and try again!");
-        }
-    });
 
 
 }
+
+
+// // Set your Stripe publishable API key here
+// Stripe.setPublishableKey('pk_test_51Hfm7wJCIpvTNXndqyN2ZsYxepcYkEsWrWTSmLoZidAZXAhheQdkRCuQKA7ylETVP8H4Rl42gEFgyGOMvpPgIpFx00yy6dHKbX');
+//
+// $(function() {
+//     var $form = $('#payment-form');
+//     $form.submit(function(event) {
+//         // Clear any errors
+//         $form.find('.has-error').removeClass('has-error')
+//
+//         // Validate the form isn't empty before requesting a token
+//         invalidForm = false;
+//         $('#payment-form input').each(function() {
+//             if ($(this).val() == '') {
+//                 $('#'+this.id).parent('.form-group').addClass('has-error');
+//                 invalidForm = true;
+//             }
+//         });
+//         if (invalidForm){
+//             return false;
+//         }
+//
+//         // Disable the submit button to prevent repeated clicks:
+//         $form.find('.submit').prop('disabled', true).html("<i class='fa fa-spinner fa-spin'></i> Making payment...");
+//
+//         // Request a token from Stripe:
+//         Stripe.card.createToken($form, stripeResponseHandler);
+//
+//         // Prevent the form from being submitted:
+//         // return false;
+//         alert("Success!")
+//         //TODO redirect user
+//     });
+//
+//     // Formatting
+//     $('#number').payment('formatCardNumber');
+//     $('#cc_exp').payment('formatCardExpiry');
+//
+//     // Determine & display the card brand
+//     $('#number').keyup(function() {
+//         card_type = $.payment.cardType($('#number').val());
+//         card_type ? $('#card_type').html("<i class='text-success fa fa-cc-"+card_type+"'></i>") : $('#card_type').html("");
+//     });
+// });
+//
+// function stripeResponseHandler(status, response) {
+//     var $form = $('#payment-form');
+//
+//     if (response.error) {
+//         // Show the errors on the form
+//         $form.find('.payment-errors').text(response.error.message).addClass('alert alert-danger');
+//         $form.find('#' + response.error.param).parent('.form-group').addClass('has-error');
+//         $form.find('button').prop('disabled', false).text('Pay $20'); // Re-enable submission
+//     }
+//     else { // Token was created!
+//         $form.find('.submit').html("<i class='fa fa-check'></i> Payment accepted");
+//         // Get the token ID:
+//         var token = response.id;
+//
+//         // Insert the token and name into the form so it gets submitted to the server:
+//         $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+//
+//         // TODO append user information to form
+//
+//         // Submit the form:
+//         $form.get(0).submit();
+//         // TODO: Create success/failure alert for clients
+//     }
+// }
 
 
 
