b11bbb3e3e751c931e1c4a3cbd6615ef80044023
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-08-15 14:35:49 -0500

    Update index.js

--- node/merchPage/index.js
+++ node/merchPage/index.js
@@ -2,164 +2,153 @@
  * Main ACM Merch file.
  *
  * Provides the backend support for merch purchases for ACM Security.
- * Config *.json5 files can be found in ./merchConfigs
  *
  * @file   index.js
  * @author Charles Rawlins.
  */
 
-// Change to select new configuration file for the front/back end
-var eventFile = "./merchConfigs/merchexample.json5"
+// Log uncaught errors in production; throw them in development.
+process.on("unhandledRejection", process.env.NODE_ENV !== "production" ?
+    console.error :
+    up => { throw up }
+);
 
-const fs = require('fs');
-var json5 = require('json5');
-var bodyParser = require('body-parser')
-const express = require('express')
-// Required to make API calls work well
-var cors = require('cors')
-const forceSync = require('sync-rpc')
-// Custom email functions for letting officers know about new registration
-var emailmod = require('./merchEmails')
-var eventSpread = require('google-spreadsheet-append-es5');
+const config = require("./merchConfigs/config");
+const emailmod = require("./merchEmails");
+const stripe = require("stripe")(config.stripeSK);
+const express = require("express");
+const cors = require("cors");
+const googleSheets = require("@googleapis/sheets");
 
-// Load config file and parse data for front and back end
-data = fs.readFileSync(eventFile);
+const auth = new googleSheets.auth.GoogleAuth({
+    keyFilename: config.googleSheetsInfo.keyFilename,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+const sheetsClientPromise = auth.getClient().then( (authClient) => {
+    return googleSheets.sheets({
+        version: "v4",
+        auth: authClient,
+    });
+});
 
-var merchInfo= json5.parse(data)
-var serverInfo = merchInfo.merch.serverInfo;
 const app = express();
 
-// Setup stripe keys
-const stripe = require("stripe")(serverInfo.stripesk);
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use("/payment", express.static("payment"));  // Host the payment page
+app.use(cors());  // Required for REST API with site
 
-// Setup configs for the server app
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors()) // Required for REST API with site
-
-//Authentication to spreadsheet for google. Used the npm google-spreadsheet-append-es5
-//Documentation here: https://www.npmjs.com/package/google-spreadsheet-append-es5
-var sheetInfo = serverInfo.googleSheetsInfo
-var spreadsheet = eventSpread({
-    auth: {
-        email: sheetInfo.authEmail,
-        keyFile: sheetInfo.keyFile
-    },
-    fileId: sheetInfo.sheetsFileID
-});
 
 /**
- * Unsorted Object array key getter.
- *
- * Searches an unorganized object array for a key and returns the value
- *
- * @param {Object}  objArray    Object array to search.
- * @param {String}  key         Key value to search for.
- * @returns {Object} value      Value of desired key.
+ * @typedef {Object} OrderInfo
+ * @property {string} customerName
+ * @property {string} email
+ * @property {number} numBundles Number of merch bundles ordered
+ * @property {"S" | "M" | "L" | "XL"} shirtSize
+ * @property {?string} discCode Discount code
+ * @property {string} transactionToken Unique Stripe payment token
  */
-function getKey(objArray,key){
-
-    for(i in objArray){
-        if (Object.keys(objArray[i])[0] === key){
-            return Object.values(objArray[i])[0]
-        }
-    }
-
-}
-
-function checkValid(valArray){
-
-    for(var i = 0; i < valArray.length; i++){
-        intVal = Math.trunc(valArray[i]) // Check for floating point numbers
-        if (intVal < 0){ // Check for negatives
-            return true
-        }
-
-    }
-    return false
-
-}
-
-/**
-* GET merch page endpoint
-*
-* GET endpoint for redirecting users to merch page. Will redirect to payment/index.html
-*/
-app.get('/merchPayPage', (req, res) => {
-    res.redirect('./payment/index.html');
-});
 
 /**
  * POST merch page endpoint
  *
  * POST endpoint for merch page. Stripe payment token will be submitted here.
- * User registration will then be added to desired spreadsheet from config file and emails
- * will be sent to officer email and new recipient.
+ * Customer's purchase will then be added to the spreadsheet listed in the
+ * config file and emails will be sent to the ACM merch email and the customer.
+ * 
+ * TODO: Enforce the maximum number of bundles on the server side.
  */
-app.post('/merchCharge', function(req, res) {
-
+app.post("/merchCharge", async (req, res) => {
     // Set the charge amount on server side for security.
-    let Chargeamounts = serverInfo.chargePrices;
-    bundlePrice = Chargeamounts[0]
+    const bundlePrice = config.chargePrices[0];
 
-    //Parse incoming data from registration page
-    userdata = req.body.data
-    merchName = userdata.merchName
-    email = userdata.email
-    numBundles = userdata.numBundles
-    shirtSize = userdata.shirtSize
-    discCode = userdata.discCode
-    token = userdata.token
+    /**
+     * @type {OrderInfo}
+     */
+    const order = req.body;
 
-    if(checkValid([numBundles])){
-        res.send(JSON.stringify({trxnSuccess:"Invalid entry!",success:false}));
+    // Parse user's data from registration page
+    const { customerName, email, numBundles, shirtSize, discCode, transactionToken } = order;
+
+    if (numBundles < 1 || numBundles % 1 !== 0) {
+        res.status(400).send({
+            trxnSuccess: "Number of bundles must be an integer greater than zero.",
+            success: false,
+        });
+        return;
     }
 
     // All merch numbers are valid, proceed with purchase
     let totalCharge = bundlePrice * numBundles;
 
-    if (discCode === serverInfo.discCode){
-        totalCharge = (bundlePrice - serverInfo.discAmount) * numBundles;
+    if (discCode === config?.discCode) {
+        totalCharge = (bundlePrice - config.discAmount) * numBundles;
     }
 
-    //Create new Stripe charge with public/private keys
-    var charge = stripe.charges.create({
-        amount: totalCharge,
-        currency: "usd",
-        source: token,
-        description: serverInfo.title,
-        receipt_email:email
-    }, function (err, charge){
-        //If charge fails, log error and let user know.
-        if(err){
-            console.log(err);
-            res.send(JSON.stringify({trxnSuccess:"Your card was declined!",success:false}));
-        }else{
-            // Charge succeeds! Log the registration in the google spreadsheet and email user/officer
-            spreadsheet.add({
-                timestamp: new Date(),
-                name:merchName,
-                email:email,
-                bundles:numBundles,
-                shirtsize: shirtSize,
-                fulfilled:'NO'
-                },
-                function(err, res){
-                    console.log(err)
-                });
+    // Create new Stripe charge with public/private keys
+    try {
+        await stripe.charges.create({
+            amount: totalCharge,
+            currency: "usd",
+            source: transactionToken,
+            description: config.title,
+            receipt_email: email,
+        });
+    } catch (error) {
+        // If the charge fails, log the error and let the user know.
+        console.error(error);
+        res.send({
+            trxnSuccess: "Your card was declined!",
+            success: false,
+        });
+        return;
+    }
 
-            console.log("Payment was successful.");
+    // Payment was successful; log the order to the spreadsheet and
+    // send an email notification to the officers.
+    console.log("Payment was successful.");
 
-            // Send the new user an email and let the officers know
-            emailmod.sendMerchEmail(merchName,email,[numBundles],shirtSize)
+    // TODO: Make it so that you don't have to await this
+    const sheets = await sheetsClientPromise;
 
-            // Let the user know that processing was successful
-            res.send(JSON.stringify({trxnSuccess:"Your payment was successful!",success:true}));
-        };
+    // Add a row to the bottom of the spreadsheet.
+    await sheets.spreadsheets.values.append({
+        spreadsheetId: config.googleSheetsInfo.spreadsheetID,
+        range: "A1",
+        valueInputOption: "RAW",
+        requestBody: {
+            majorDimension: "ROWS",
+
+            // Values are placed in the row from left to right.
+            values: [[
+                (new Date()).toString(),  // Purchase timestamp
+                customerName,
+                email,
+                numBundles,
+                shirtSize,
+                "NO",  // Fulfilled status. Always initally "NO", then
+                       // manually set to "YES" by an officer once the
+                       // order is fulfilled.
+            ]],
+        }
     });
 
+    emailmod.sendMerchEmail(
+        customerName,
+        email,
+        numBundles,
+        shirtSize
+    );
+
+    // Let the user know that processing was successful
+    res.send({
+        trxnSuccess: "Your payment was successful!",
+        success: true,
+    });
 });
+
 
 // Setup port options for starting backend
 const port = process.env.PORT || 3002; // Leave as 3002 for merch payment, allowing processes to be taken down
-app.listen(port,()=> console.log(`Listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`) );
